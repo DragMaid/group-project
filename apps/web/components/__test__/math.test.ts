53d5898ba5a54be8b1d8aab11ee1f38ef6b0bb1a
VuHoangHaiBinh <vhoanghaibinh@gmail.com>
2025-05-09 09:45:08 +0800

    Add example test format

---
+++ apps/web/components/__test__/math.test.ts
@@ -0,0 +1,16 @@
+import { add, subtract } from '../math';
+
+describe('Math functions', () => {
+  test('adds 1 + 2 to equal 3', () => {
+    expect(add(1, 2)).toBe(3);
+  });
+
+  test('subtracts 5 - 3 to equal 2', () => {
+    expect(subtract(5, 3)).toBe(2);
+  });
+
+  test('subtracts 3 - 5 to equal -2', () => {
+    expect(subtract(3, 5)).toBe(-2);
+  });
+});
+
