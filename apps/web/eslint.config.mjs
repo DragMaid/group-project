e255539de767675051c15c756501771054c4b897
VuHoangHaiBinh <vhoanghaibinh@gmail.com>
2025-05-13 15:46:40 +0800

    extend new base config files in @web

---
+++ apps/web/eslint.config.mjs
@@ -0,0 +1,4 @@
+import eslintBaseConfig from "eslintconfig/eslint.base.config.mjs";
+import eslintNextConfig from "eslintconfig/eslint.next.config.mjs";
+
+export default [...eslintBaseConfig, ...eslintNextConfig];
