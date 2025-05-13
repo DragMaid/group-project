53dcf201e6a1accdd386679e9cbaca49914fa0c0
VuHoangHaiBinh <vhoanghaibinh@gmail.com>
2025-05-13 15:45:15 +0800

    updae tsupconfig to not build when there are no ts files

--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -1,13 +1,27 @@
 import { defineConfig } from "tsup";
+import fs from "fs";
 
-export default defineConfig({
-  entry: ["components/**/*.{ts,tsx}"],
-  format: ["esm"],
-  dts: true,
-  sourcemap: true,
-  clean: true,
-  outDir: "dist",
-  target: "esnext",
-  watch: process.env.DEV === "true",
-  external: ["react", "react-dom"],
+const hasSourceFiles =
+  fs.existsSync("src") &&
+  fs
+    .readdirSync("src")
+    .some((file) => file.endsWith(".ts") || file.endsWith(".tsx"));
+
+export default defineConfig(() => {
+  if (!hasSourceFiles) {
+    //console.log("No source files found. Skipping build.");
+    return [];
+  }
+
+  return {
+    entry: ["./components/**/*.{ts,tsx}"],
+    format: ["esm"],
+    dts: true,
+    sourcemap: true,
+    clean: true,
+    outDir: "dist",
+    target: "esnext",
+    watch: process.env.DEV === "true",
+    external: ["react", "react-dom"],
+  };
 });
